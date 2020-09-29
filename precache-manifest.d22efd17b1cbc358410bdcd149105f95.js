821bf01f9818c9a491d1b6e0798d42d0c6c4e1e9
tejaulli <teja.ulli123@gmail.com>
2020-09-29 14:54:54 +0530

    1dfmd

---
+++ precache-manifest.d22efd17b1cbc358410bdcd149105f95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a31397d52054ece7b600c51de6c04b",
+    "url": "/spiderIndia/index.html"
+  },
+  {
+    "revision": "368f442f1342c1e31a10",
+    "url": "/spiderIndia/static/css/main.5af733e9.chunk.css"
+  },
+  {
+    "revision": "7831b1c6c3f409ae3392",
+    "url": "/spiderIndia/static/js/2.cdd2e068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spiderIndia/static/js/2.cdd2e068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368f442f1342c1e31a10",
+    "url": "/spiderIndia/static/js/main.01a257f8.chunk.js"
+  },
+  {
+    "revision": "e0b267dc024be625fd2d",
+    "url": "/spiderIndia/static/js/runtime-main.df92e87c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spiderIndia/static/media/logo.5d5d9eef.svg"
+  }
+]);
